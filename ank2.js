fae69c1eedd985c58e6ee7e8b0344154c4a39b31
Ro777 <romka256@yandex.ru>
2020-03-04 17:47:39 +0300

    final

--- ank2.js
+++ ank2.js
@@ -1,33 +1,36 @@
 $(document).ready(function () {
-    $('.q2-7,.q3-7,.q4-7,.form-q2,.form-q3,.form-q4,#block-2').hide();
+  $('.q2-7,.q3-7,.q4-7,.form-q2,.form-q3,.form-q4,#block-2').hide();
+
+  var bl = $('div[class^="q1_"]');
+  var lb = $("div[class^='q5_']");
 
     $('.form-sub1').click(function () {
-        let part = $('q9');
-      let url = window.location;
-      part.val = url
-        $('#block-1,#text').hide();
-        $('#block-2').fadeIn();
-        $('.q6-7,.q7-7,.q8-7').hide();
-      })
+      if ($('input[id^="q2-"]').is(':checked') || $('input[id^="q3-"]').is(':checked') || $('input[id^="q4-"]').is(':checked')) {
+      $('#block-1,#text').hide();
+      $('#block-2').fadeIn();
+      $('.q6-7,.q7-7,.q8-7').hide();
 
-    var bl = $('div[class^="q1_"]');
-    var lb = $("div[class^='q5_']");
 
-    $(bl).click(function () {
-        $(bl).css({
-            'backgroundColor': '#fff',
-            'color': '#434647'
+      let url = window.location.search.slice(1);
+      $('#9').attr('value', url);
+
+
+      $(lb).click(function () {
+        $(lb).css({
+          'backgroundColor': '#fff',
+          'color': '#434647'
         })
-    
-        $(bl).removeClass('active');
 
-        var col = this.style.borderColor;
+        $(lb).removeClass('active');
+
+        var col = window.getComputedStyle(this, null).getPropertyValue('border-color');
         $(this).css({
-            'backgroundColor': col,
-            'color': '#fff'
+          'backgroundColor': col,
+          'color': '#fff'
         })
-        $('.q1_5').css({
-            'color': '#434647'
+
+        $('.q5_5').css({
+          'color': '#434647'
         })
         $(this).addClass('active')
 
@@ -37,40 +40,116 @@ $(document).ready(function () {
         /*var la = parseInt($('div[class~="active"]+input > label').html());*/
 
         switch (true) {
-            case (num <= 6):
-                $('.form-q2,.form-q3').hide();
-                $('.form-q4').fadeIn();
-                break;
+          case (num <= 6):
+            $('.form-q6,.form-q7').hide();
+            $('.form-q8').fadeIn();
+            break;
 
-            case (num <= 8):
-                $('.form-q2,.form-q4').hide();
-                $('.form-q3').fadeIn();
-                break;
+          case (num <= 8):
+            $('.form-q6,.form-q8').hide();
+            $('.form-q7').fadeIn();
+            break;
 
-            case (num >= 9):
-                $('.form-q3,.form-q4').hide();
-                $('.form-q2').fadeIn();
-                break;
+          case (num >= 9):
+            $('.form-q7,.form-q8').hide();
+            $('.form-q6').fadeIn();
+            break;
 
-            default:
-                $('.form-q2,.form-q3,.form-q4').show();
+          default:
+            $('.form-q6,.form-q7,.form-q8').show();
         }
 
+        $(".q6-6,.q7-6,.q8-6").click(function () {
+
+          if ($("#q6-6").is(':checked'))
+            $('.q6-7').slideDown();
+          else $('.q6-7').slideUp();
+
+          if ($("#q7-6").is(':checked'))
+            $('.q7-7').slideDown();
+          else $('.q7-7').slideUp();
+
+          if ($("#q8-6").is(':checked'))
+            $('.q8-7').slideDown();
+          else $('.q8-7').slideUp();
+        });
+      });
+    }
+      else
+      return false;
+    });
+
+
+
+
+ 
+
+  $(bl).click(function () {
+    $(bl).css({
+      'backgroundColor': '#fff',
+      'color': '#434647'
     })
 
-    $(".q2-6,.q3-6,.q4-6").click(function () {
+    $(bl).removeClass('active');
 
-        if ($("#q2-6").is(':checked'))
-            $('.q2-7').slideDown();
-        else $('.q2-7').slideUp();
+    var col = window.getComputedStyle(this, null).getPropertyValue('border-color');
+    $(this).css({
+      'backgroundColor': col,
+      'color': '#fff'
+    })
+    $('.q1_5').css({
+      'color': '#434647'
+    })
 
-        if ($("#q3-6").is(':checked'))
-            $('.q3-7').slideDown();
-        else $('.q3-7').slideUp();
+    $(this).addClass('active')
 
-        if ($("#q4-6").is(':checked'))
-            $('.q4-7').slideDown();
-        else $('.q4-7').slideUp();
-    });
+    var la = $(this).find('.lab');
+    var num = parseInt(la.html());
+
+    /*var la = parseInt($('div[class~="active"]+input > label').html());*/
+
+    switch (true) {
+      case (num <= 6):
+        $('.form-q2,.form-q3').hide();
+        $('.form-q4').fadeIn();
+        break;
+
+      case (num <= 8):
+        $('.form-q2,.form-q4').hide();
+        $('.form-q3').fadeIn();
+        break;
+
+      case (num >= 9):
+        $('.form-q3,.form-q4').hide();
+        $('.form-q2').fadeIn();
+        break;
+
+      default:
+        $('.form-q2,.form-q3,.form-q4').show();
+    }
+
+  });
+
+  $(".q2-6,.q3-6,.q4-6").click(function () {
+
+    if ($("#q2-6").is(':checked'))
+      $('.q2-7').slideDown();
+    else $('.q2-7').slideUp();
+
+    if ($("#q3-6").is(':checked'))
+      $('.q3-7').slideDown();
+    else $('.q3-7').slideUp();
+
+    if ($("#q4-6").is(':checked'))
+      $('.q4-7').slideDown();
+    else $('.q4-7').slideUp();
+  });
+
+  $('.form-sub2').click(function() {
+    if ($('input[id^="q6-"]').is(':checked') || $('input[id^="q7-"]').is(':checked') || $('input[id^="q8-"]').is(':checked'))
+      return true
+    else 
+    return false
+  })
 
 });
